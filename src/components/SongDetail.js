ef3503302c92229e5fa32af2aafdcc483a916189
Zeyang Yi <danielzeyangyi@gmail.com>
2019-07-20 23:41:58 -0400

    finish

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -2,7 +2,15 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 const SongDetail = ({ song }) => {
-  return song ? <div>{song.title}</div> : <div>Select a song!</div>;
+  return song ? (
+    <div>
+      <h3>Details for:</h3>
+      <p>Title: {song.title}</p>
+      <p>Duration: {song.duration}</p>
+    </div>
+  ) : (
+    <div>Select a song!</div>
+  );
 };
 
 const mapStateToProps = state => {
